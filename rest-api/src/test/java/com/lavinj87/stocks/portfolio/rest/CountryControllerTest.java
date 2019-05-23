28521176c9ab7aa1d91c568d6e0c2051df7bd88e
lavinj87 <7030463+lavinj87@users.noreply.github.com>
2019-05-23 23:39:27 +0200

    Done configuring Travis CI

--- rest-api/src/test/java/com/lavinj87/stocks/portfolio/rest/CountryControllerTest.java
+++ rest-api/src/test/java/com/lavinj87/stocks/portfolio/rest/CountryControllerTest.java
@@ -63,7 +63,7 @@ public class CountryControllerTest {
                 .andReturn()
                 .getResponse().getContentAsString();
 
-        String expected = "{\"countryId\":\"ES\",\"name\":\"Spain_foo\"}";
+        String expected = "{\"countryId\":\"ES\",\"name\":\"Spain\"}";
 
         Assert.assertEquals(expected, actual);
     }
