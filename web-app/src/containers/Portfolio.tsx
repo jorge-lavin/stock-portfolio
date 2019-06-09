3be07b93fc4a320f6fc86c02614fd90e4b722786
lavinj87 <7030463+lavinj87@users.noreply.github.com>
2019-06-09 17:46:39 +0200

    Components and Container separation begins. Portfolios routes properly

--- web-app/src/containers/Portfolio.tsx
+++ web-app/src/containers/Portfolio.tsx
@@ -1,18 +1,24 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { RouteComponentProps } from 'react-router';
 
-import NormalizedObjects from '../store/NormalizedObjects'
+
 import { ApplicationState, ConnectedReduxProps } from '../store'
+import NormalizedObjects from '../store/NormalizedObjects'
+import { Dividend } from '../store/dividends/types'
 import { Portfolio } from '../store/portfolios/types'
 import { fetchRequest as portfoliosFetchRequest } from '../store/portfolios/actions'
-import PortfoliosComponent from '../components/Portfolios';
+import PortfolioComponent from '../components/Portfolio';
 
 
 // Separate state props + dispatch props to their own interfaces.
-interface PropsFromState {
-  loading: boolean
-  portfolios: { entities: NormalizedObjects<Portfolio>, lastFetched: Date }
-  errors?: string
+interface MatchParams {
+  portfolioId: string;
+}
+
+interface PropsFromState extends RouteComponentProps<MatchParams> {
+  portfolios:{ entities: NormalizedObjects<Portfolio>, lastFetched: Date } ,
+  dividends: { entities: NormalizedObjects<Dividend>, lastFetched: Date }
 }
 
 // We can use `typeof` here to map our dispatch types to the props, like so.
@@ -26,13 +32,15 @@ type AllProps = PropsFromState & PropsFromDispatch & ConnectedReduxProps
 
 class PortfoliosContainer extends React.Component<AllProps> {  
   public componentDidMount() {
-      this.props.portfoliosFetchRequest()
+      //this.props.portfoliosFetchRequest()
+      // TODO Fetch portfolio and dividends if needed ...
   }
 
   public render() {
-    const { portfolios, loading, errors } = this.props
+    const { portfolios, dividends, match } = this.props
+    const portfolio = portfolios.entities.byId[match.params.portfolioId]
     return (
-      <PortfoliosComponent portfolios={portfolios} loading={loading} errors={errors} />
+      <PortfolioComponent portfolio={portfolio} dividends={dividends} />
     )
   }
 }
@@ -40,9 +48,10 @@ class PortfoliosContainer extends React.Component<AllProps> {
 // It's usually good practice to only include one context at a time in a connected component.
 // Although if necessary, you can always include multiple contexts. Just make sure to
 // separate them from each other to prevent prop conflicts.
-const mapStateToProps = ({ portfolios }: ApplicationState) => ({
-  loading: portfolios.loading,
-  errors: portfolios.errors,
+const mapStateToProps = ({ dividends, portfolios }: ApplicationState) => ({
+  loading: dividends.loading || portfolios.loading,
+  errors: dividends.errors || portfolios.errors,
+  dividends: { entities: dividends.entities, lastFetched: dividends.lastFetched },
   portfolios: { entities: portfolios.entities, lastFetched: portfolios.lastFetched }
 })
 
